ab798900da58641aea57d98ce6de7e783ae98910
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2019-06-24 20:47:32 -0400

    Add framework for help command

--- bot.js
+++ bot.js
@@ -1,14 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
+const config = require('./config.json');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+client.on('message', async message => {
+
+  //Ignore other bots
+  if (message.author.bot) return;
+
+  //Ignore messages not using our prefix
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  //Separate arguments from the command
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === "help") {
+    message.channel.send("this is the help");
   }
 });
 

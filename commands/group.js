f81ded7dc2209c35c03fe0d91b7f23c862faf8e5
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2019-06-25 22:12:29 -0400

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -7,8 +7,12 @@ const func = require('../functions.js');
 //Main loop for executing command
 module.exports.run = async(client, message, args) => {
   if (args[1] === "create") {
-    //need to save group creator
-    //need to save group name
+    var group = {
+      creator: message.author,
+      name: args[2],
+      game: args[3],
+      time: args[4] + "T" + args[5]
+    }
   }
 }
 
@@ -17,6 +21,6 @@ module.exports.config = {
     name: 'group',
     aliases: ['team', "teamup", "squad"],
     description: 'used to create or manage a group.',
-    usage: 'group create [name] [game] [time], group join [name], group leave [name], group disband [name]',
+    usage: 'group create [name] [game] [date] [time], group join [name], group leave [name], group disband [name]',
     noalias: "No Aliases"
 }

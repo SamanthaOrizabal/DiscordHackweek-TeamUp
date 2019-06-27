0e5c68cf684e669c3fc86d66b2cb2b3d14e4ea9a
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2019-06-27 01:17:52 -0400

    Added ability to join group in db

--- commands/group.js
+++ commands/group.js
@@ -17,7 +17,23 @@ module.exports.run = async(client, message, args) => {
     if (dateTime === false) {
       message.channel.send("Please specify the date and time of your group's meeting time.");
       return;
-    } 
+    }
+
+    //verify a group with the submitted name isn't already in db
+    // COMBAK: I cant figure out why this isn't working... HELP???
+    /*Models.Group.find({ name: args[1] }, function(err, docs) {
+      if (err) {
+        console.error(err);
+      }
+      console.log(docs);
+      console.log(docs.length);
+      console.log(docs.length !== 0);
+      if (docs.lenth) {
+        message.channel.send("A group with this name already exists! join them with \"?group join " + args[1] + "\" or choose a different name.");
+        return;
+      }
+    });*/
+
 
     var group = new Models.Group({
       creator: message.author,
@@ -40,13 +56,38 @@ module.exports.run = async(client, message, args) => {
 
     //save this to the database, and remember to include server it was created on
 
-  } else if (args[1] === "join") { //group join [name]
+  } else if (args[0] === "join") { //group join [name]
     //find group with name == args[1] in this server/message channel
     //add message.author t0 participants list
-  } else if (args[1] === "leave") { //group leave [name]
+    // COMBAK: We need to prevent players from joining if they are already in the group
+    Models.Group.findOne({ name: args[1], server: message.guild.id }, function(err, docs) {
+      if (err) {
+        console.error(err)
+        return;
+      }
+      if (docs == null) {
+        message.channel.send("That group doesn't exist");
+        return;
+      }
+      docs.participants.push(message.author);
+      docs.save(function(error) {
+        if (error) {
+          console.error(error);
+          message.channel.send("**ERROR:** " + error.message);
+          return;
+        } else {
+          console.log("Group successfully saved into mongodb.");
+          message.channel.send('You joined the group.');
+        }
+      });
+    });
+
+
+  } else if (args[0] === "leave") { //group leave [name]
     //find group with name == args[1] in this server/message channel
     //remove message.author from participants list
-  } else if (args[1] === "disband") { //group disband [name]
+
+  } else if (args[0] === "disband") { //group disband [name]
     //find group with name == args[1] in this server/message channel
     //confirm message.author is group creator
     //delete group
